a247e43be007b09ff02dc808f2a79f3dd8f27d63
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-10-28 23:45:11 +0700

    feat: Add HelloWorld source generator

---
+++ Tests/TimeWarp.SourceGenerators.TestConsole/Generated/TimeWarp.SourceGenerators/TimeWarp.SourceGenerators.HelloWorldGenerator/HelloWorld.g.cs
@@ -0,0 +1,7 @@
+﻿// TW0001: The HelloWorld generator has been loaded and initialized
+namespace TimeWarp.Generated;
+
+public static class HelloWorld 
+{
+  public const string Message = "Hello from TimeWarp Source Generator!";
+}
